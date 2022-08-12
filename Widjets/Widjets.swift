cf65498e721a626e8c2868b101f9483ebeda1470
ilya Biryuk <iiya.ps4.com@gmail.com>
2022-08-12 23:51:36 +0300

    We're not far from widgets!

--- Widjets/Widjets.swift
+++ Widjets/Widjets.swift
@@ -8,13 +8,15 @@
 import WidgetKit
 import SwiftUI
 
+var names = ["ilya","vanya","rodion"]
+
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), names: [])
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), names: [])
         completion(entry)
     }
 
@@ -23,26 +25,37 @@ struct Provider: TimelineProvider {
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
+        let entryDate = currentDate
+        let entry = SimpleEntry(date: entryDate, names: names)
+        entries.append(entry)
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: entries, policy: .never)
         completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    var names: [String]
 }
 
 struct WidjetsEntryView : View {
     var entry: Provider.Entry
 
+    @ViewBuilder
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack {
+            ForEach(entry.names, id: \.self) { name in
+                HStack{
+                    Spacer()
+                    Text(name)
+                    Spacer()
+                }
+            }
+            HStack{
+                Image(systemName: "plus")
+            }
+        }
     }
 }
 
@@ -61,7 +74,7 @@ struct Widjets: Widget {
 
 struct Widjets_Previews: PreviewProvider {
     static var previews: some View {
-        WidjetsEntryView(entry: SimpleEntry(date: Date()))
+        WidjetsEntryView(entry: SimpleEntry(date: Date(), names: names))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
