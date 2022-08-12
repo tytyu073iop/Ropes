f55a211bae22afd26f4fd6b56efede04258c23d0
ilya Biryuk <iiya.ps4.com@gmail.com>
2022-08-12 16:51:37 +0300

    watch connectivity

--- Ropes/Core data/Task core data.swift
+++ Ropes/Core data/Task core data.swift
@@ -35,6 +35,7 @@ extension ToDo : Identifiable {
         self.date = Date.now
         self.id = id
         PersistenceController.save()
+        WC.shared.send("Tasks", [self])
         if auto {
             PushNotfication(time: defaults.double(forKey: "time"))
         }
--- RopesForWatch Watch App/WatchConnectivity.swift
+++ RopesForWatch Watch App/WatchConnectivity.swift
@@ -21,10 +21,35 @@ final class WC : NSObject {
             session.activate()
         }
     }
+    func send(_ name : String,_ message: [Any]) {
+            guard WCSession.default.activationState == .activated else {
+              return
+            }
+            #if os(iOS)
+            guard WCSession.default.isWatchAppInstalled else {
+                return
+            }
+            #else
+            guard WCSession.default.isCompanionAppInstalled else {
+                return
+            }
+            #endif
+            
+            WCSession.default.sendMessage([name : message], replyHandler: nil) { error in
+                print("Cannot send message: \(String(describing: error))")
+            }
+        }
     //aw
 }
 
 extension WC: WCSessionDelegate {
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+            if let notificationText = message["Tasks"] as? ToDo {
+                DispatchQueue.main.async { [weak self] in
+                    ToDo(context: PersistenceController.shared.container.viewContext , name: notificationText.name)
+                }
+            }
+        }
     func session(_ session: WCSession,
                  activationDidCompleteWith activationState: WCSessionActivationState,
                  error: Error?) {}
