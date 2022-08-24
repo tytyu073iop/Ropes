7478e6b56c8e586b51d271fe6e450de46bfaeb26
ilya Biryuk <iiya.ps4.com@gmail.com>
2022-08-24 11:08:51 +0300

    update

---
+++ qqqq Watch App/qqqqApp.swift
@@ -0,0 +1,17 @@
+//
+//  qqqqApp.swift
+//  qqqq Watch App
+//
+//  Created by Илья Бирюк on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct qqqq_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
