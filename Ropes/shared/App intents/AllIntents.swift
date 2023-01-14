e5db93e73b698614f8bf9154ee3102f09c10a28e
ilya Biryuk <iiya.ps4.com@gmail.com>
2023-01-15 01:23:52 +0300

    localize

--- Ropes/shared/App intents/AllIntents.swift
+++ Ropes/shared/App intents/AllIntents.swift
@@ -11,8 +11,8 @@ import AppIntents
 
 @available(iOS 16.0, macOS 13.0, watchOS 9.0, *) struct RopesShortCuts: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
-        AppShortcut(intent: ShowTasks(), phrases: ["Show \(.applicationName)", "Show my \(.applicationName)"]) ;
-        AppShortcut(intent: AddRopeShortcut(), phrases: ["Create a \(.applicationName)", "Create the \(.applicationName)", "Create \(.applicationName)", "make a \(.applicationName)", "make the \(.applicationName)", "make \(.applicationName)"])
+        AppShortcut(intent: AddRopeShortcut(), phrases: ["Create a \(.applicationName)", "Create the \(.applicationName)", "Create \(.applicationName)", "make a \(.applicationName)", "make the \(.applicationName)", "make \(.applicationName)"]) ;
+        AppShortcut(intent: ShowTasks(), phrases: ["Show \(.applicationName)", "Show my \(.applicationName)"])
     }
     static var shortcutTileColor: ShortcutTileColor = .purple
 }
