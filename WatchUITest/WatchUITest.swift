5f652234ee5bcd06f88d4cd8980e73bf28792c4c
tytyu073iop <iiya.ps4.com@gmail.com>
2023-08-09 14:35:36 +0300

    added notfication handler

--- WatchUITest/WatchUITest.swift
+++ WatchUITest/WatchUITest.swift
@@ -11,6 +11,11 @@ import XCTest
 final class WatchInterfaceUITest: XCTestCase {
 
     override func setUpWithError() throws {
+        addUIInterruptionMonitor(withDescription: "Notfication") {
+            $0.swipeUp()
+            $0.buttons["Allow"].firstMatch.tap()
+            return true
+        }
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
